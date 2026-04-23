f3b38c887e95e95fa04c8e6d7e234eaa694a3fdc
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-04-22 19:31:53 -0600

    Actualizar MobHUD.java

--- MobHUD.java
+++ MobHUD.java
@@ -7,15 +7,35 @@ public class MobHUD extends JavaPlugin {
     
     private static MobHUD instance;
     private HudManager hudManager;
+    private ConfigManager configManager;
     
     @Override
     public void onEnable() {
         instance = this;
+        
+        // Cargar configuración primero
+        configManager = new ConfigManager(this);
         hudManager = new HudManager(this);
         
-        getServer().getPluginManager().registerEvents(new EntityListener(hudManager), this);
+        // Registrar eventos
+        getServer().getPluginManager().registerEvents(new EntityListener(this), this);
         
-        getLogger().info("§aMobHUD v1.0 activado - ¡Mostrando vida y EXP de mobs!");
+        // Registrar comando de recarga
+        getCommand("mobhud").setExecutor((sender, command, label, args) -> {
+            if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
+                if (sender.hasPermission("mobhud.reload") || sender.isOp()) {
+                    configManager.recargar();
+                    sender.sendMessage(configManager.getMensajeRecargado());
+                } else {
+                    sender.sendMessage("§cNo tienes permiso.");
+                }
+                return true;
+            }
+            sender.sendMessage("§eMobHUD v1.0 - /mobhud reload");
+            return true;
+        });
+        
+        getLogger().info("§aMobHUD v1.0 activado correctamente.");
     }
     
     @Override
@@ -23,11 +43,7 @@ public class MobHUD extends JavaPlugin {
         getLogger().info("§cMobHUD desactivado.");
     }
     
-    public static MobHUD getInstance() {
-        return instance;
-    }
-    
-    public HudManager getHudManager() {
-        return hudManager;
-    }
+    public static MobHUD getInstance() { return instance; }
+    public HudManager getHudManager() { return hudManager; }
+    public ConfigManager getConfigManager() { return configManager; }
 }
